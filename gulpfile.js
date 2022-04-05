a11f0538be0f38694c22b42d6017a0d17fbcb353
WAGMI Times <wagmitimes@gmail.com>
2022-04-05 17:35:48 -0300

    :zap: Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+var gulp    = require('gulp'),
+    sass    = require('gulp-sass')(require('sass')),
+    connect = require('gulp-connect'),
+    pug     = require('gulp-pug'),
+    plumber = require('gulp-plumber'),
+    rename  = require('gulp-rename'),
+    uglify  = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer');
+
+function reload(done) {
+    connect.server({
+        root: 'public',
+        livereload: true,
+        port: 8080
+    });
+    done();
+}
+
+function styles() {
+    return (
+        gulp.src('src/sass/main.sass')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('public/css'))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('public/css'))
+        .pipe(connect.reload())
+    );
+}
+
+function scripts() {
+    return (
+        gulp.src('src/js/scripts.js')
+        .pipe(plumber())
+        .pipe(gulp.dest('public/js'))
+        .pipe(uglify())
+        .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(connect.reload())
+    );
+}
+
+function html() {
+    return (
+        gulp.src('src/*.html')
+        .pipe(plumber())
+        .pipe(connect.reload())
+    );
+}
+
+function views() {
+    return (
+        gulp.src('src/**/*.pug')
+        .pipe(plumber())
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('public/'))
+        .pipe(connect.reload())
+    )
+}
+
+function watchTask(done) {
+    gulp.watch('src/*.html', html);
+    gulp.watch('src/sass/**/*.sass', styles);
+    gulp.watch('src/js/**/*.js', scripts);
+    gulp.watch('src/**/**/*.pug', views);
+    done();
+}
+
+const watch = gulp.parallel(watchTask, reload);
+const build = gulp.series(gulp.parallel(styles, scripts, html, views));
+
+exports.reload = reload;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.html = html;
+exports.views = views;
+exports.watch = watch;
+exports.build = build;
+exports.default = watch;
