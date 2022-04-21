b5a7de4b473cc2c1a432f0b0407ec5055ce82e9e
WAGMI Times <wagmitimes@gmail.com>
2022-04-21 18:54:19 -0300

    :zap: Organize magic.js

--- src/js/magic.js
+++ src/js/magic.js
@@ -1,25 +1,46 @@
-//////////////////////////////// 
-// Add Bills
-//////////////////////////////// 
-const addBills = document.querySelector('.modal-wrapper .add-bills');
+/**************************************************
+ * DOM Elements
+ *************************************************/
+
+// Form to Add Bills
+const addBillsForm = document.querySelector('.modal-wrapper .add-bills');
+
+// Transaction (Bills) List
 const txList = document.querySelector('.tx-list');
 
 
-addBills.addEventListener('submit', (e) => {
+
+
+
+/**************************************************
+ * DOM Events
+ *************************************************/
+
+
+/**
+ * Sends Form
+ * 
+ * TODO: Add multiple bills behavior — will keep modal open when adding new bills
+ * TODO: Add single bill behavior — will add bill and close the modal
+ */
+addBillsForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const bill = {
     paid: false,
-
     desc: (document.querySelector('[name=description]')).value,
     amount: (document.querySelector('[name=amount]')).value,
     category: (document.querySelector('[name=category]')).value,
     duedate: (document.querySelector('[name=duedate]')).value,
     spender: (document.querySelector('[name=spenders]')).value
   };
-
+  
   addBill(bill);
+
+  // Resets form
   addBills.reset();
+
+  //TODO: Focus on amount
 });
 
 
