1dd2c6d5cb5677d56ed91932db41319411cebb75
WAGMI Times <wagmitimes@gmail.com>
2022-04-21 16:10:56 -0300

    :zap: Set up base functions for adding Bills

---
+++ src/js/bills.js
@@ -0,0 +1,13 @@
+function addBill(data) {} // Adds bill: to DOM && localStorage
+
+function removeBill(bill) {} // Removes a specific bill: from DOM & localStorage
+
+function addToLocalStorage(bills) {} // Adds bills to localStorage
+
+function getFromLocalStorage() {} // Gets bills from localStorage
+
+
+
+function toggleOpen(el, className = '--open') {} // Toggles open (anything).
+
+function togglePaid(el) {} // Updates bill to paid or unpaid
